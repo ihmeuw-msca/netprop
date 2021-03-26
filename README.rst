0aeca5ddc9096e389701ff73271e74638324da87
zhengp0 <zhengp@uw.edu>
2021-03-25 19:19:20 -0700

    initialize the repo

---
+++ README.rst
@@ -0,0 +1,2 @@
+Network Propotional Analysis
+============================
---
+++ src/netprop/__about__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "netprop"
+__summary__ = "Network proptional analysis"
+__uri__ = "https://github.com/ihmeuw-msca/netprop"
+
+__version__ = "0.0.0"
+
+__author__ = "Peng Zheng"
+__email__ = "zhengp@uw.edu"
+
+__license__ = "BSD 2-Clause License"
+__copyright__ = f"Copyright 2021 {__author__}"
