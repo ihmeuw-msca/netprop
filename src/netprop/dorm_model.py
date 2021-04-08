63306a9f86fea07f4373b0788b7b24400415cdf9
Peng Zheng <zhengp@uw.edu>
2021-04-07 19:33:20 -0700

    Add priors (#4)
    * add gaussian and uniform prior into the dormmodel
    
    * fix minor issues
    
    * link priors to model
    
    * update examples to use priors
    
    * open up the solver method
    
    * update example comment on solver option

--- src/netprop/dorm_model.py
+++ src/netprop/dorm_model.py
@@ -1,4 +1,6 @@
-from typing import Iterable
+from typing import Dict, Iterable, Union
+from operator import attrgetter
+import numpy as np
 from numpy import ndarray
 from netprop.data import Data
 
@@ -10,13 +12,47 @@ class DormModel:
 
     def __init__(self,
                  name: str,
-                 covs: Iterable[str]):
+                 covs: Iterable[str],
+                 uprior: Dict[str, Iterable[float]] = None,
+                 gprior: Dict[str, Iterable[float]] = None):
         self.name = name
         self.covs = list(covs)
+        self.uprior = uprior
+        self.gprior = gprior
 
     @property
     def size(self) -> int:
         return len(self.covs)
+
+    def get_prior(self,
+                  prior_info: Union[Dict[str, Iterable[float]], None],
+                  default_prior: Iterable[float]) -> ndarray:
+        prior = np.repeat(np.asarray(default_prior)[:, None], self.size, axis=1)
+        if prior_info is not None:
+            for k, v in prior_info.items():
+                if k in self.covs:
+                    prior[:, self.covs.index(k)] = v
+        return prior
+
+    uprior = property(attrgetter("_uprior"))
+
+    @uprior.setter
+    def uprior(self, uprior_info: Union[Dict[str, Iterable[float]], None]):
+        default_uprior = [-np.inf, np.inf]
+        if uprior_info is not None:
+            for p in uprior_info.values():
+                assert p[0] <= p[1], "Uniform prior lower bound <= upper bound."
+        self._uprior = self.get_prior(uprior_info, default_uprior)
+
+    gprior = property(attrgetter("_gprior"))
+
+    @gprior.setter
+    def gprior(self, gprior_info: Union[Dict[str, Iterable[float]], None]):
+        default_gprior = [0.0, np.inf]
+        if gprior_info is not None:
+            for p in gprior_info.values():
+                assert p[1] > 0, "Gaussian prior sd must be positive."
+        self._gprior = self.get_prior(gprior_info, default_gprior)
 
     def get_mat(self, data: Data) -> ndarray:
         return data[self.covs]
